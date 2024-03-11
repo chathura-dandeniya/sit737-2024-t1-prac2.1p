b452c89ac2d7436ac6e578f1b1192add0eb62e6a
chathura-dandeniya <s222489357@deakin.edu.au>
2024-03-11 21:26:10 +1100

    Add modification

--- server.js
+++ server.js
@@ -1,33 +1,11 @@
-// Load the HTTP module to create an HTTP server.
-const http = require('http');
+const express = require('express'); // Import the Express.js module
+const app = express(); // Create an instance of Express.js
+const port = 3000; // Define the port number
 
-// Load the file system module to read files from the file system.
-const fs = require('fs');
+// Serve static files from the 'public' directory (you can name this directory anything you like)
+app.use(express.static('public'));
 
-// Define the port number on which the server will listen for requests.
-const port = 3000;
-
-// Create an HTTP server. The function passed to createServer is called once for each HTTP request made to the server.
-const server = http.createServer((req, res) => {
-    // Read the content of 'index.html' asynchronously.
-    fs.readFile('index.html', (err, data) => {
-        // Check if there was an error reading the file.
-        if (err) {
-            // If an error occurred, send an HTTP status code of 500 (Internal Server Error).
-            res.writeHead(500);
-            // End the response and send a message indicating the error.
-            res.end('Error loading index.html');
-        } else {
-            // If the file was read successfully, send an HTTP status code of 200 (OK).
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            // End the response and send the content of 'index.html'.
-            res.end(data);
-        }
-    });
-});
-
-// Make the server start listening on the specified port. Once the server starts listening, the callback function is called.
-server.listen(port, () => {
-    // Log a message to the console indicating that the server is running and listening for requests.
+// Start the server and listen on the defined port
+app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
